b287780eb4bfd7a2b0135b6027b66b572daa8d6e
Akash B R <eleven011-AN>
2026-07-17 19:29:47 +0530

    updating pws method

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,25 +1,41 @@
-const CACHE_NAME = "attendance-system-v1";
+const CACHE_NAME = "attendance-system-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/page1.html",
-  "/page2.html",
-  "/page3.html",
-  "/page4.html",
-  "/manage.html",
-  "/promotion.html",
-  "/registeration.html",
-  "/teachpage1.html",
-  "/teachpage2.html",
-  "/teachpage3.html"
+  "./",
+  "./index.html",
+  "./page1.html",
+  "./page2.html",
+  "./page3.html",
+  "./page4.html",
+  "./manage.html",
+  "./promotion.html",
+  "./registeration.html",
+  "./teachpage1.html",
+  "./teachpage2.html",
+  "./teachpage3.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
